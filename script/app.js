2eef5764c8e262401f3820c6bc82e1c055de6620
Bethuel Khisa <sunguribethuel@gmail.com>
2024-07-10 16:17:17 +0300

    1. Add Utility functions 2. Add DOMContentLoaded event listener 3. Added an array 4. Add functions to display items

---
+++ script/app.js
@@ -0,0 +1,39 @@
+const createElement = (element) => {
+  return document.createElement(element);
+};
+
+const addText = (element, text) => {
+  return (element.innerText = text);
+};
+
+const appendChild = (child, parent) => {
+  return parent.appendChild(child);
+};
+
+const select = (selector) => {
+  return document.querySelector(selector);
+};
+
+const listen = (element, event, callback) => {
+  return element.addEventListener(event, callback);
+};
+
+const addAttribute = (element, attribute, content) => {
+  return element.addAttribute(attribute, content);
+};
+
+const shoppingList = ["Milk", "Tea"];
+
+const ol = select("ol");
+
+const displayItems = () => {
+  shoppingList.forEach(createAListItem);
+};
+
+listen(document, "DOMContentLoaded", displayItems);
+
+const createAListItem = (item) => {
+  const li = createElement("li");
+  addText(li, item);
+  appendChild(li, ol);
+};
